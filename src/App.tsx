49c47766107398559e166d70bfe987e25305cfb7
sh-web <shfujishim@fab.pasona.tech>
2024-09-11 16:25:29 +0900

    type表示済み

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 interface Pokemon {
   name: string;
@@ -8,12 +9,28 @@ interface Details {
   name: string;
   id: number;
   img: string;
+  types: {
+    slot: number;
+    type: {
+      name: string;
+      url: string;
+    };
+  }[];
 }
+type Filtering = {
+  id: number;
+  name: string;
+  type: string;
+};
 function fetchHome() {
+  const { register, handleSubmit } = useForm<Filtering>();
+  const onSubmit = (data: Filtering) => {
+    console.log(data);
+  };
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [details, setDetails] = useState<Details[]>([]);
   const [urls, setUrls] = useState<string[]>([]);
-  let limit = 20;
+  let limit = 151;
   const [offset, setOffset] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
   let response = {};
@@ -42,6 +59,7 @@ function fetchHome() {
             name: data.name,
             id: data.id,
             img: data.sprites.front_default,
+            types: data.types,
           };
         })
       );
@@ -56,12 +74,32 @@ function fetchHome() {
   return (
     <>
       <h1>モンスターブック</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>ポケモン名</label>
+
+        <input id="name" {...register("name")} />
+
+        <label>図鑑No.</label>
+        <input id="id" {...register("id")} />
+        <label>タイプ</label>
+        <input id="type" {...register("type")} />
+        <button type="submit">フィルター</button>
+      </form>
+
       {details.map((detail) => (
         <div className="cardstyle" key={detail.id}>
           <img src={detail.img} />
           <p>
             図鑑No.{detail.id}:{detail.name}
           </p>
+          {detail.types.length === 1 && (
+            <p>タイプ:{detail.types[0].type.name}</p>
+          )}
+          {detail.types.length === 2 && (
+            <p>
+              タイプ:{detail.types[0].type.name}・{detail.types[1].type.name}
+            </p>
+          )}
         </div>
       ))}
     </>
